17dc9e91affdcc91bb636670e6fd39d0dac7ee32
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-04-23 21:46:33 -0700

    got most of the layout done with the image working correctly

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,35 +1,47 @@
 import React from "react";
 import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image";
 
 
 const Navbar = () =>{
     return(
     <>
-        <div className="navContainer">
             <nav
-            className="flex items-center p-3 flex-wrap"
-            >   
-                <div>
+            className="navContainer md:flex md:items-center md:justify-between"
+            >   <div>
+                    <span>
+                        <StaticImage
+                            className="ml-10"
+                            src="../images/CodeForSanJose.png"
+                            layout="fixed"
+                            alt="Code For San Jose Logo"
+                            quality={95}
+                            height={35}
+                        />
+                    </span>
+                </div>
+            <ul className="md:flex md:item-center">
+                <li className="mx-6 mt-5 font-semibold ourWork">
                     <Link>Our Work</Link>
-                </div>
-                <div>
+                </li>
+                <li className="mx-6 mt-5 font-semibold">
                     <Link>Get Involved</Link>
-                </div>
-                <div>
+                </li>
+                <li className="mx-6 mt-5 font-semibold">
                     <Link>Events</Link>
-                </div>
-                <div>
+                </li>
+                <li className="mx-6 mt-5 font-semibold">
                     <Link>About Us</Link>
-                </div>
-                <div>
+                </li>
+                <li className="mx-6">
                     <Link>
                     <button className="text-white m-2 p-3 font-semibold text-sm rounded-full donate">
                         Donate
                     </button>
                     </Link>
-                </div>
+                </li>
+            </ul>
             </nav>
-        </div>
         </>
     )
 }
