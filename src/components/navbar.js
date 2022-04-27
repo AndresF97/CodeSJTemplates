12dcd70e14a4f6d9cb87a91dfd90a3655c1cf2e6
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-04-27 16:35:06 -0700

    Navbar responsiveness on the works

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image";
+import { Helmet } from "react-helmet";
 
 
 const Navbar = () =>{
     return(
-    <>
+    <>  
+        <Helmet>
+            <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+            <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+        </Helmet>
             <nav
             className="navContainer md:flex md:items-center md:justify-between"
-            >   <div>
+            >   <div className="flex justify-between items-center">
                     <span>
                         <StaticImage
                             className="ml-10"
@@ -19,8 +24,11 @@ const Navbar = () =>{
                             height={35}
                         />
                     </span>
+                    <span className="text-md cursor-pointer md:hidden block mx-2">
+                        <ion-icon name="menu-outline"></ion-icon>
+                    </span>
                 </div>
-            <ul className="md:flex md:item-center">
+            <ul className=" md:flex md:item-center z-[-1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500 " id="mini-background">
                 <li className="mx-6 mt-5 font-semibold ourWork">
                     <Link>Our Work</Link>
                 </li>
@@ -35,7 +43,7 @@ const Navbar = () =>{
                 </li>
                 <li className="mx-6">
                     <Link>
-                    <button className="text-white m-2 p-3 font-semibold text-sm rounded-full donate">
+                    <button className="text-white m-2 p-3 font-semibold text-sm rounded-full" id="donate">
                         Donate
                     </button>
                     </Link>
